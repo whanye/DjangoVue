e5e7b9c07db07abc24b437b14d39860eb0f031fe
Eric McAllister <emcallister98@gmail.com>
2020-10-23 03:21:07 -0400

    added cateogry section and moved products into it with key

--- storeProject/apps/core/views.py
+++ storeProject/apps/core/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from apps.store.models import Product
+
 
 # Create your views here.
 def frontpage(request):
-    return render(request, 'frontpage.html')
+    products = Product.objects.all() #Get all the products in dict
+    context = {
+        'products': products
+    }
+    return render(request, 'frontpage.html', context)
 
 def contact(request):
     return render(request, 'contact.html')
